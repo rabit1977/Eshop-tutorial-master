300a31aef08267610aee3347ab1759e2f180e78c
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-05-26 23:01:04 +0200

    ... commiting changeds

--- Eshop-tutorial-master/backend/multer.js
+++ Eshop-tutorial-master/backend/multer.js
@@ -1,17 +1,38 @@
-const multer = require("multer");
-const path = require("path");
+// // Use ES6 import syntax instead of require
+// import multer from "multer";
+// import path from "path";
+
+// // Use arrow functions instead of regular functions
+// const storage = multer.diskStorage({
+//   destination: (req, res, cb) => {
+//     // Use path.resolve instead of path.join for absolute paths
+//     cb(null, path.resolve(__dirname, "./uploads"));
+//   },
+//   filename: (req, file, cb) => {
+//     // Use template literals instead of string concatenation
+//     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+//     const filename = file.originalname.split(".")[0];
+//     cb(null, `${filename}-${uniqueSuffix}.png`);
+//   },
+// });
+
+// Use export default instead of exports
+// export default multer({ storage: storage });
+
+/* older code is down */
+
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: function (req,res,cb){
-        cb(null, path.join(__dirname, "./uploads"));
-    },
-    filename: function (req,file,cb) {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        const filename = file.originalname.split(".")[0];
-        cb(null,filename + "-" + uniqueSuffix + ".png");
-    },
+  destination: function (req, res, cb) {
+    cb(null, path.join(__dirname, './uploads'));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const filename = file.originalname.split('.')[0];
+    cb(null, filename + '-' + uniqueSuffix + '.png');
+  },
 });
 
-exports.upload = multer({storage: storage});
-
-
+exports.upload = multer({ storage: storage });
--- Eshop-tutorial-master/frontend/src/redux/reducers/cart.js
+++ Eshop-tutorial-master/frontend/src/redux/reducers/cart.js
@@ -1,17 +1,17 @@
 // Import the createReducer and createSlice functions from Redux Toolkit
-import { createReducer, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 // Define the initial state of the cart
 const initialState = {
-  cart: localStorage.getItem("cartItems")
-    ? JSON.parse(localStorage.getItem("cartItems"))
+  cart: localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
     : [],
 };
 
 // Create a slice of the state for the cart
 const cartSlice = createSlice({
   // Give the slice a name
-  name: "cart",
+  name: 'cart',
   // Pass the initial state
   initialState,
   // Define the reducers for the slice
@@ -22,7 +22,9 @@ const cartSlice = createSlice({
       const isItemExist = state.cart.find((i) => i._id === item._id);
       if (isItemExist) {
         // Update the existing item in the cart
-        state.cart = state.cart.map((i) => (i._id === isItemExist._id ? item : i));
+        state.cart = state.cart.map((i) =>
+          i._id === isItemExist._id ? item : i
+        );
       } else {
         // Add the new item to the cart
         state.cart.push(item);
@@ -40,17 +42,13 @@ const cartSlice = createSlice({
 export const { addToCart, removeFromCart } = cartSlice.actions;
 export const cartReducer = cartSlice.reducer;
 
-
-
-
 /* Older Code down - refactored by me */
-
 
 // import { createReducer } from "@reduxjs/toolkit";
 
 // const initialState = {
 //   cart: localStorage.getItem("cartItems")
-//     ? JSON.parse(localStorage.getItem("cartItems"))
+    // ? JSON.parse(localStorage.getItem("cartItems"))
 //     : [],
 // };
 
@@ -78,6 +76,3 @@ export const cartReducer = cartSlice.reducer;
 //     };
 //   },
 // });
-
-
-
